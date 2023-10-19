3843f0ae9a87b214850b51d51162adf0fd7f4734
Poppy <transruler8@gmail.com>
2023-10-19 10:16:22 -0600

    sdfsdf
    sdgdfg

---
+++ CARS 4/sketch.js
@@ -0,0 +1,15 @@
+// Cars 4
+// Matiachuk, Skyler
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
